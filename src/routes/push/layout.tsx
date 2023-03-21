49ef18f611fefca9c0c18a235e5a4275cc06e624
odanzhou <odanzhou@gmail.com>
2023-03-21 12:04:24 +0800

    feat: 增加轮训推送

--- src/routes/push/layout.tsx
+++ src/routes/push/layout.tsx
@@ -1,18 +1,33 @@
 import { Outlet, Link } from '@modern-js/runtime/router';
 
+const list = [
+  {
+    name: 'http2',
+    url: '/push/http2',
+  },
+  {
+    name: 'scoket',
+    url: '/push/scoket',
+  },
+  {
+    name: 'event source',
+    url: '/push/eventSource',
+  },
+  {
+    name: 'poll',
+    url: '/push/poll',
+  },
+];
+
 export default () => {
   return (
     <div>
       <div>
-        <div>
-          <Link to="/push/http2">http2</Link>
-        </div>
-        <div>
-          <Link to="/push/scoket">scoket</Link>
-        </div>
-        <div>
-          <Link to="/push/eventSource">event source</Link>
-        </div>
+        {list.map(({ name, url }) => (
+          <div key={name}>
+            <Link to={url}>{name}</Link>
+          </div>
+        ))}
       </div>
       <Outlet />
     </div>
---
+++ src/routes/push/poll/page.tsx
@@ -0,0 +1,9 @@
+const PollPush = () => {
+  return (
+    <div>
+      <h2>轮训 推送</h2>
+    </div>
+  );
+};
+
+export default PollPush;
